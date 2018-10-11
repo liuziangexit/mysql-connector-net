d22652036ece49f8d2f48d10b0bdc57f92e22c26
liuziangexit <liuziang@liuziangexit.com>
2018-10-11 11:13:48 +0800

    Add new Connection String option "CACertificateFile"
    This option specifies the path to a CA certificate file in a PEM Encoded (.pem) format. This should be used in with SslMode=VerifyCA or SslMode=VerifyFull to enable verification of a CA certificate that is not trusted by the Operating System's certificate store.
    
    https://stackoverflow.com/questions/52695107/how-to-add-a-custom-ssl-verification-to-mysql-connector-net/52695823 SOLVED.

--- MySQL.Data/src/common/Ssl.cs
+++ MySQL.Data/src/common/Ssl.cs
@@ -33,145 +33,149 @@ using System.Text;
 
 namespace MySql.Data.Common
 {
-  internal class Ssl
-  {
-    private MySqlConnectionStringBuilder settings;
-    private static Dictionary<string, SslProtocols> tlsConnectionRef = new Dictionary<string, SslProtocols>();
-    private static Dictionary<string, int> tlsRetry = new Dictionary<string, int>();
-    private static SslProtocols[] tlsProtocols = new SslProtocols[] { SslProtocols.Tls12, SslProtocols.Tls11 };
-    private static Object thisLock = new Object();
-
-    public Ssl(MySqlConnectionStringBuilder settings)
+    internal class Ssl
     {
-      this.settings = settings;
-    }
+        private MySqlConnectionStringBuilder settings;
+        private static Dictionary<string, SslProtocols> tlsConnectionRef = new Dictionary<string, SslProtocols>();
+        private static Dictionary<string, int> tlsRetry = new Dictionary<string, int>();
+        private static SslProtocols[] tlsProtocols = new SslProtocols[] { SslProtocols.Tls12, SslProtocols.Tls11 };
+        private static Object thisLock = new Object();
 
-    /// <summary>
-    /// Retrieve client SSL certificates. Dependent on connection string 
-    /// settings we use either file or store based certificates.
-    /// </summary>
-    private X509CertificateCollection GetClientCertificates()
-    {
-      X509CertificateCollection certs = new X509CertificateCollection();
-
-      // Check for file-based certificate
-      if (settings.CertificateFile != null)
-      {
-        X509Certificate2 clientCert = new X509Certificate2(settings.CertificateFile,
-            settings.CertificatePassword);
-        certs.Add(clientCert);
-        return certs;
-      }
-
-      if (settings.CertificateStoreLocation == MySqlCertificateStoreLocation.None)
-        return certs;
-
-      StoreLocation location =
-          (settings.CertificateStoreLocation == MySqlCertificateStoreLocation.CurrentUser) ?
-          StoreLocation.CurrentUser : StoreLocation.LocalMachine;
-
-      // Check for store-based certificate
-      X509Store store = new X509Store(StoreName.My, location);
-      store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
-
-
-      if (settings.CertificateThumbprint == null)
-      {
-        // Return all certificates from the store.
-        certs.AddRange(store.Certificates);
-        return certs;
-      }
-
-      // Find certificate with given thumbprint
-      certs.AddRange(store.Certificates.Find(X509FindType.FindByThumbprint,
-                settings.CertificateThumbprint, true));
-
-      if (certs.Count == 0)
-      {
-        throw new MySqlException("Certificate with Thumbprint " +
-           settings.CertificateThumbprint + " not found");
-      }
-      return certs;
-    }
-
-
-    public MySqlStream StartSSL(ref Stream baseStream, Encoding encoding, string connectionString)
-    {
-      RemoteCertificateValidationCallback sslValidateCallback =
-          new RemoteCertificateValidationCallback(ServerCheckValidation);
-      SslStream ss = new SslStream(baseStream, false, sslValidateCallback, null);
-      X509CertificateCollection certs = GetClientCertificates();
-
-      string connectionId = connectionString.GetHashCode().ToString();
-      SslProtocols tlsProtocol = SslProtocols.Tls;
-
-      lock (thisLock)
-      {
-        if (tlsConnectionRef.ContainsKey(connectionId))
+        public Ssl(MySqlConnectionStringBuilder settings)
         {
-          tlsProtocol = tlsConnectionRef[connectionId];
+            this.settings = settings;
         }
-        else
+
+        /// <summary>
+        /// Retrieve client SSL certificates. Dependent on connection string 
+        /// settings we use either file or store based certificates.
+        /// </summary>
+        private X509CertificateCollection GetClientCertificates()
         {
-          if (!tlsRetry.ContainsKey(connectionId))
-          {
-            tlsRetry[connectionId] = 0;
-          }
-          for (int i = tlsRetry[connectionId]; i < tlsProtocols.Length; i++)
-          {
-            tlsProtocol |= tlsProtocols[i];
-          }
-        }
-        try
-        {
-          ss.AuthenticateAsClientAsync(settings.Server, certs, tlsProtocol, false).Wait();
-          tlsConnectionRef[connectionId] = tlsProtocol;
-          tlsRetry.Remove(connectionId);
-        }
-        catch(AggregateException ex)
-        {
-          if (ex.GetBaseException() is IOException)
-          {
-            tlsConnectionRef.Remove(connectionId);
-            if (tlsRetry.ContainsKey(connectionId))
+            X509CertificateCollection certs = new X509CertificateCollection();
+
+            // Check for file-based certificate
+            if (settings.CertificateFile != null)
             {
-              if (tlsRetry[connectionId] > tlsProtocols.Length)
-                throw new MySqlException(Resources.SslConnectionError, ex);
-              tlsRetry[connectionId] += 1;
+                X509Certificate2 clientCert = new X509Certificate2(settings.CertificateFile,
+                    settings.CertificatePassword);
+                certs.Add(clientCert);
+                return certs;
             }
-          }
-          throw ex.GetBaseException();
+
+            if (settings.CertificateStoreLocation == MySqlCertificateStoreLocation.None)
+                return certs;
+
+            StoreLocation location =
+                (settings.CertificateStoreLocation == MySqlCertificateStoreLocation.CurrentUser) ?
+                StoreLocation.CurrentUser : StoreLocation.LocalMachine;
+
+            // Check for store-based certificate
+            X509Store store = new X509Store(StoreName.My, location);
+            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+
+            if (settings.CertificateThumbprint == null)
+            {
+                // Return all certificates from the store.
+                certs.AddRange(store.Certificates);
+                return certs;
+            }
+
+            // Find certificate with given thumbprint
+            certs.AddRange(store.Certificates.Find(X509FindType.FindByThumbprint,
+                      settings.CertificateThumbprint, true));
+
+            if (certs.Count == 0)
+            {
+                throw new MySqlException("Certificate with Thumbprint " +
+                   settings.CertificateThumbprint + " not found");
+            }
+            return certs;
         }
-      }
 
-      baseStream = ss;
-      MySqlStream stream = new MySqlStream(ss, encoding, false);
-      stream.SequenceByte = 2;
 
-      return stream;
+        public MySqlStream StartSSL(ref Stream baseStream, Encoding encoding, string connectionString)
+        {
+            SslStream ss = new SslStream(baseStream, false, new RemoteCertificateValidationCallback(ServerCheckValidation), null);
+            X509CertificateCollection certs = GetClientCertificates();
+
+            string connectionId = connectionString.GetHashCode().ToString();
+            SslProtocols tlsProtocol = SslProtocols.Tls;
+
+            lock (thisLock)
+            {
+                if (tlsConnectionRef.ContainsKey(connectionId))
+                {
+                    tlsProtocol = tlsConnectionRef[connectionId];
+                }
+                else
+                {
+                    if (!tlsRetry.ContainsKey(connectionId))
+                    {
+                        tlsRetry[connectionId] = 0;
+                    }
+                    for (int i = tlsRetry[connectionId]; i < tlsProtocols.Length; i++)
+                    {
+                        tlsProtocol |= tlsProtocols[i];
+                    }
+                }
+                try
+                {
+                    ss.AuthenticateAsClientAsync(settings.Server, certs, tlsProtocol, false).Wait();
+                    tlsConnectionRef[connectionId] = tlsProtocol;
+                    tlsRetry.Remove(connectionId);
+                }
+                catch (AggregateException ex)
+                {
+                    if (ex.GetBaseException() is IOException)
+                    {
+                        tlsConnectionRef.Remove(connectionId);
+                        if (tlsRetry.ContainsKey(connectionId))
+                        {
+                            if (tlsRetry[connectionId] > tlsProtocols.Length)
+                                throw new MySqlException(Resources.SslConnectionError, ex);
+                            tlsRetry[connectionId] += 1;
+                        }
+                    }
+                    throw ex.GetBaseException();
+                }
+            }
+
+            baseStream = ss;
+            MySqlStream stream = new MySqlStream(ss, encoding, false);
+            stream.SequenceByte = 2;
+
+            return stream;
+        }
+
+        private bool ServerCheckValidation(object sender, X509Certificate certificate,
+                                                  X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            if (settings.SslMode == MySqlSslMode.Preferred ||
+                settings.SslMode == MySqlSslMode.Required)
+            {
+                //Tolerate all certificate errors.
+                return true;
+            }
+
+            if ((settings.SslMode == MySqlSslMode.VerifyCA
+                || settings.SslMode == MySqlSslMode.VerifyFull)
+                && settings.CACertificateFile != null)
+            {
+                var result = chain.ChainStatus.Length == 1
+                    && chain.ChainStatus[0].Status == X509ChainStatusFlags.UntrustedRoot
+                    && chain.ChainElements[chain.ChainElements.Count - 1].Certificate.Thumbprint.Equals(new X509Certificate2(settings.CACertificateFile).Thumbprint);
+                if (settings.SslMode == MySqlSslMode.VerifyCA)
+                    return result && !sslPolicyErrors.HasFlag(SslPolicyErrors.RemoteCertificateNotAvailable);
+                if (settings.SslMode == MySqlSslMode.VerifyFull)
+                    return result && sslPolicyErrors == SslPolicyErrors.RemoteCertificateNameMismatch;
+            }
+
+            return false;
+        }
     }
-
-    private bool ServerCheckValidation(object sender, X509Certificate certificate,
-                                              X509Chain chain, SslPolicyErrors sslPolicyErrors)
-    {
-      if (sslPolicyErrors == SslPolicyErrors.None)
-        return true;
-
-      if (settings.SslMode == MySqlSslMode.Preferred ||
-          settings.SslMode == MySqlSslMode.Required)
-      {
-        //Tolerate all certificate errors.
-        return true;
-      }
-
-      if (settings.SslMode == MySqlSslMode.VerifyCA &&
-          sslPolicyErrors == SslPolicyErrors.RemoteCertificateNameMismatch)
-      {
-        // Tolerate name mismatch in certificate, if full validation is not requested.
-        return true;
-      }
-
-      return false;
-    }
-  }
 }
